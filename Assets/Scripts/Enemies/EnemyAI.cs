1a6025df9928c3367fcff25669059bc8afe3115b
JordiItb <jordi.blanco.7e3@itb.cat>
2021-05-19 19:08:06 +0200

    Jail with Enemies

--- Assets/Scripts/Enemies/EnemyAI.cs
+++ Assets/Scripts/Enemies/EnemyAI.cs
@@ -11,29 +11,18 @@ public class EnemyAI : MonoBehaviour
     public float rotationSpeed;
     int currentPoint;
     Vector3 velocity = Vector3.zero;
-    int currentAction;
-    int wander = 0;
-    int follow = 1;
     
     void Start()
     {
         currentPoint = 0;
         target = GameObject.Find("Player");
-        currentAction = 0;
     }
 
     
     void Update()
     {
         
-        switch(currentAction){
-            case 0:
-                Wander();
-                break;
-            case 1:
-                Follow();
-                break;
-        }
+        Wander();
         
     }
 
@@ -63,31 +52,11 @@ public class EnemyAI : MonoBehaviour
 
     }
 
-    void Follow(){
-
-        //Move to the target point
-        transform.position = Vector3.SmoothDamp(transform.position, trackPoints[currentPoint].position, ref velocity, walkSpeed, walkSpeed);
-            
-        // Determine which direction to rotate towards
-        Vector3 targetDirection = new Vector3(trackPoints[currentPoint].position.x - transform.position.x, 0f, trackPoints[currentPoint].position.z - transform.position.z);
-
-        // The step size is equal to speed times frame time.
-        float singleStep = rotationSpeed * Time.deltaTime;
-
-        // Rotate the forward vector towards the target direction by one step
-        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
-
-        // Draw a ray pointing at our target in
-        Debug.DrawRay(transform.position, newDirection, Color.red);
-
-        // Calculate a rotation a step closer to the target and applies rotation to this object
-        transform.rotation = Quaternion.LookRotation(newDirection);
-
-    }
-
     void OnTriggerEnter(Collider collision){
 
-        if(collision.gameObject.layer == 13){
+        Debug.Log(collision.gameObject);
+
+        if(collision.gameObject.layer == 13 && collision.gameObject == trackPoints[currentPoint].gameObject){
             ChangePosition();
         }
 
